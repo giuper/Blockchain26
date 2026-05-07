918d062601738c905d5c8573ce82873f7395993e
Giuseppe Persiano <giuper@gmail.com>
2026-05-07 13:11:35 +0200

    Step by Step scripts

--- 05-AtomicGroups/StepByStep/Step1-A1.py
+++ 05-AtomicGroups/StepByStep/Step1-A1.py
@@ -1,20 +1,20 @@
 import sys
 from algosdk.v2client import algod
-from algosdk.future.transaction import AssetTransferTxn, write_to_file
+from algosdk.transaction import AssetTransferTxn, write_to_file
 from utilities import algodAddress, algodToken
 
 TXFolder="TX/"
 
-def step1(pk1,pk2,algodClient):
+def step1(pk1,pk2,algodClient,assetIDX):
 
     params=algodClient.suggested_params()
     txn1=AssetTransferTxn(
-        sender=pk1,sp=params,receiver=pk2,amt=4,index=737294246)
+        sender=pk1,sp=params,receiver=pk2,amt=4,index=assetIDX)
     write_to_file([txn1],TXFolder+"step1A1.utx")
 
 if __name__=="__main__":
-    if (len(sys.argv)!=3):
-        print("Usage: python3.10 "+sys.argv[0]+" <addr1 sending asset> <addr2 sending algo>")
+    if (len(sys.argv)!=4):
+        print("Usage: python "+sys.argv[0]+" <addr1 sending asset> <addr2 sending algo> <asset index>")
         exit()
 
     account1=sys.argv[1]
@@ -23,6 +23,7 @@ if __name__=="__main__":
         pk1=f.read()[:58]
     with open(account2,'r') as f:
         pk2=f.read()[:58]
+    assetIDX=int(sys.argv[3])
 
     algodClient=algod.AlgodClient(algodToken,algodAddress)
-    step1(pk1,pk2,algodClient)
+    step1(pk1,pk2,algodClient,assetIDX)
---
+++ 05-AtomicGroups/StepByStep/Step1-A2.py
@@ -0,0 +1,29 @@
+import sys
+from algosdk.v2client import algod
+from algosdk.transaction import PaymentTxn, write_to_file
+from utilities import algodAddress, algodToken
+
+TXFolder="TX/"
+
+def step1(pk1,pk2,algodClient):
+
+    params=algodClient.suggested_params()
+
+##account2 pays account1 1 Algo 
+    txn2=PaymentTxn(
+        sender=pk2,sp=params,receiver=pk1,amt=1_000_000)
+    write_to_file([txn2],TXFolder+"step1A2.utx")
+
+if __name__=="__main__":
+    if (len(sys.argv)!=3):
+        print("Usage: python3.10 "+sys.argv[0]+" <addr1 sending asset> <addr2 sending algo>")
+        exit()
+
+    account1=sys.argv[1]
+    account2=sys.argv[2]
+    with open(account1,'r') as f:
+        pk1=f.read()[:58]
+    with open(account2,'r') as f:
+        pk2=f.read()[:58]
+    algodClient=algod.AlgodClient(algodToken,algodAddress)
+    step1(pk1,pk2,algodClient)
