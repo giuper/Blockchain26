830b043d9f219b5f751f20f7bb8e1ac9cc3bcf0f
Giuseppe Persiano <giuper@gmail.com>
2026-05-08 12:19:31 +0200

    Update Step2-Alice.py

--- 05-AtomicGroups/StepByStep/Step2-Alice.py
+++ 05-AtomicGroups/StepByStep/Step2-Alice.py
@@ -19,7 +19,7 @@ def cgid(fileTxAlice,fileTxBob,fileMnemAlice):
     txnBob.group=gid
     write_to_file([txnAlice],TXFolder+"AliceWithGid.utx")
     print("Alice unsigned txn with gid in file",TXFolder+"AliceWithGid.utx")
-    write_to_file([txnBob],TXFolder+"BibWithGid.utx")
+    write_to_file([txnBob],TXFolder+"BobWithGid.utx")
     print("Bob unsigned txn with gid in file",TXFolder+"BobWithGid.utx")
 
     stxnAlice=txnAlice.sign(skAlice)
