2f5b0d16cb776327b59d9ae28f17fec2aa18f950
giuper <giuper@gmail.com>
2026-06-02 08:47:35 +0200

    starting with creation+optin

--- 10-DAO/dao.py
+++ 10-DAO/dao.py
@@ -1,6 +1,6 @@
 import sys
 from pyteal import *
-from daoutilities import DAOtokenName, DAOGovName
+from daoutilities import DAOtokenName, DAOGovName, DAOTokenAsset, DAOGovAsset
 
 cmd=ScratchVar(TealType.bytes)
 
@@ -16,31 +16,65 @@ def handle_start():
                                 TxnField.type_enum: TxnType.AssetConfig,
                                 TxnField.config_asset_total: Int(1_000_000),
                                 TxnField.config_asset_decimals: Int(3),
-                                TxnField.config_asset_name: Bytes(DAOtokenName),
-                                TxnField.config_asset_unit_name: Bytes("B26"),
-                                TxnField.config_asset_url: Bytes("https://github.com/giuper/Blockchain26"),
+                                TxnField.config_asset_name: Bytes(DAOTokenName),
+                                TxnField.config_asset_unit_name: Bytes(DAOTokenUnit)
+                                TxnField.config_asset_url: Bytes(DAOURL)
                                 TxnField.config_asset_manager: Global.current_application_address(),
                                 TxnField.config_asset_reserve: Global.current_application_address(),
                                 TxnField.config_asset_freeze: Global.current_application_address(),
                                 TxnField.config_asset_clawback: Global.current_application_address()
                                 }),
                                 InnerTxnBuilder.Submit(),
-                                App.globalPut(Bytes("assetIDToken"),InnerTxn.created_asset_id()),
+                                App.globalPut(Bytes("IDToken"),InnerTxn.created_asset_id()),
+
                             InnerTxnBuilder.Begin(),
                             InnerTxnBuilder.SetFields({
                                 TxnField.type_enum: TxnType.AssetConfig,
-                                TxnField.config_asset_total: Int(3),
+                                TxnField.config_asset_total: Int(1),
                                 TxnField.config_asset_decimals: Int(0),
-                                TxnField.config_asset_unit_name: Bytes("B26G"),
-                                TxnField.config_asset_name: Bytes(DAOGovName),
-                                TxnField.config_asset_url: Bytes("https://github.com/giuper/Blockchain26"),
+                                TxnField.config_asset_unit_name: Concat(Bytes(DAOGovName),Bytes("1"))
+                                TxnField.config_asset_name: Bytes(DAOGovUnit),
+                                TxnField.config_asset_url: Bytes(DAOURL)
                                 TxnField.config_asset_manager: Global.current_application_address(),
                                 TxnField.config_asset_reserve: Global.current_application_address(),
                                 TxnField.config_asset_freeze: Global.current_application_address(),
                                 TxnField.config_asset_clawback: Global.current_application_address()
                             }),
                             InnerTxnBuilder.Submit(),
-                            App.globalPut(Bytes("assetIDGov"),InnerTxn.created_asset_id()),
+                            App.globalPut(Bytes("IDGov1"),InnerTxn.created_asset_id()),
+
+                            InnerTxnBuilder.Begin(),
+                            InnerTxnBuilder.SetFields({
+                                TxnField.type_enum: TxnType.AssetConfig,
+                                TxnField.config_asset_total: Int(1),
+                                TxnField.config_asset_decimals: Int(0),
+                                TxnField.config_asset_unit_name: Concat(Bytes(DAOGovName),Bytes("2"))
+                                TxnField.config_asset_name: Bytes(DAOGovUnit),
+                                TxnField.config_asset_url: Bytes(DAOURL)
+                                TxnField.config_asset_manager: Global.current_application_address(),
+                                TxnField.config_asset_reserve: Global.current_application_address(),
+                                TxnField.config_asset_freeze: Global.current_application_address(),
+                                TxnField.config_asset_clawback: Global.current_application_address()
+                            }),
+                            InnerTxnBuilder.Submit(),
+                            App.globalPut(Bytes("IDGov2"),InnerTxn.created_asset_id()),
+
+                            InnerTxnBuilder.Begin(),
+                            InnerTxnBuilder.SetFields({
+                                TxnField.type_enum: TxnType.AssetConfig,
+                                TxnField.config_asset_total: Int(1),
+                                TxnField.config_asset_decimals: Int(0),
+                                TxnField.config_asset_unit_name: Concat(Bytes(DAOGovName),Bytes("3"))
+                                TxnField.config_asset_name: Bytes(DAOGovUnit),
+                                TxnField.config_asset_url: Bytes(DAOURL)
+                                TxnField.config_asset_manager: Global.current_application_address(),
+                                TxnField.config_asset_reserve: Global.current_application_address(),
+                                TxnField.config_asset_freeze: Global.current_application_address(),
+                                TxnField.config_asset_clawback: Global.current_application_address()
+                            }),
+                            InnerTxnBuilder.Submit(),
+                            App.globalPut(Bytes("IDGov3"),InnerTxn.created_asset_id()),
+
                             Approve()
                         ])).Else(Reject())
     return h_start
@@ -61,7 +95,7 @@ def handle_priceTok(prefix):
             TxnField.type_enum: TxnType.AssetTransfer,
             TxnField.asset_receiver: Txn.sender(),
             TxnField.asset_amount: Int(1),
-            TxnField.xfer_asset: App.globalGet(Bytes("assetIDGov"))
+            TxnField.xfer_asset: App.globalGet(Bytes("IDGov"))
         }),
         InnerTxnBuilder.Submit(),
         Approve()])
@@ -89,86 +123,60 @@ def approval_program(Alice,Bob,Charlie):
                     App.globalPut(Bytes("assetIDToken"),Int(0)),
                     Approve()])
 
-    handle_optin=If(Or(Txn.sender()==Alice,
-                       Txn.sender()==Bob,
-                       Txn.sender()==Charlie)).Then(
-                    Seq([
-                            InnerTxnBuilder.Begin(),
-                            InnerTxnBuilder.SetFields({
-                                TxnField.type_enum: TxnType.AssetTransfer,
+    handle_optin=Cond(
+		[Txn.sender()==Alice,Seq([
+                	InnerTxnBuilder.Begin(),
+                        InnerTxnBuilder.SetFields({
+                        	TxnField.type_enum: TxnType.AssetTransfer,
                                 TxnField.asset_receiver: Txn.sender(),
                                 TxnField.asset_amount: Int(1),
-                                TxnField.xfer_asset: App.globalGet(Bytes("assetIDGov"))
-                            }),
-                            InnerTxnBuilder.Submit(),
-                            Approve()
-                    ])).Else(Approve())
+                                TxnField.xfer_asset: App.globalGet(Bytes("IDGov1"))
+                        }),
+                        InnerTxnBuilder.Submit(),
+                        Approve()])
+		],
+		[Txn.sender()==Bob,Seq([
+                	InnerTxnBuilder.Begin(),
+                        InnerTxnBuilder.SetFields({
+                        	TxnField.type_enum: TxnType.AssetTransfer,
+                                TxnField.asset_receiver: Txn.sender(),
+                                TxnField.asset_amount: Int(1),
+                                TxnField.xfer_asset: App.globalGet(Bytes("IDGov2"))
+                        }),
+                        InnerTxnBuilder.Submit(),
+                        Approve()])
+		],
+		[Txn.sender()==Charlie,Seq([
+                	InnerTxnBuilder.Begin(),
+                        InnerTxnBuilder.SetFields({
+                        	TxnField.type_enum: TxnType.AssetTransfer,
+                                TxnField.asset_receiver: Txn.sender(),
+                                TxnField.asset_amount: Int(1),
+                                TxnField.xfer_asset: App.globalGet(Bytes("IDGov3"))
+                        }),
+                        InnerTxnBuilder.Submit(),
+                        Approve()])
+		],
+    	)
 
     handle_closeout=Approve()
 
-    handle_updateapp=If(Or(Txn.sender()==Alice,
-                           Txn.sender()==Bob,
-                           Txn.sender()==Charlie)).Then(Approve()).Else(Reject())
-
-    handle_deleteapp=If(Or(Txn.sender()==Alice,
-                           Txn.sender()==Bob,
-                           Txn.sender()==Charlie)).Then(
-                             Seq([
-                                InnerTxnBuilder.Begin(),
-                                    InnerTxnBuilder.SetFields({
-                                        TxnField.type_enum: TxnType.AssetConfig,
-                                        TxnField.config_asset: App.globalGet(Bytes("assetIDGov"))
-                                    }),
-                                InnerTxnBuilder.Submit(),
-                                InnerTxnBuilder.Begin(),
-                                    InnerTxnBuilder.SetFields({
-                                        TxnField.type_enum: TxnType.AssetConfig,
-                                        TxnField.config_asset: App.globalGet(Bytes("assetIDToken"))
-                                    }),
-                                InnerTxnBuilder.Submit(),
-                                InnerTxnBuilder.Begin(),
-                                    InnerTxnBuilder.SetFields({
-                                        TxnField.type_enum: TxnType.Payment,
-                                        TxnField.amount: Int(0),
-                                        TxnField.receiver: Charlie,
-                                        TxnField.close_remainder_to: Charlie,
-                                    }),
-                                InnerTxnBuilder.Submit(),
-                                Approve()])).Else(Reject())
-
-    handle_buy=If(And(Global.group_size()==Int(2),
-                  Gtxn[0].type_enum()==TxnType.Payment,
-                  Gtxn[0].receiver()==Global.current_application_address(),
-                  Gtxn[0].amount()>=Btoi(Gtxn[1].application_args[1])*App.globalGet(Bytes("scurrentPrice")))
-            ).Then(Seq([
-                     InnerTxnBuilder.Begin(),
-                     InnerTxnBuilder.SetFields({
-                         TxnField.type_enum: TxnType.AssetTransfer,
-                         TxnField.asset_receiver: Txn.sender(),
-                         TxnField.asset_amount: Btoi(Gtxn[1].application_args[1]),
-                         TxnField.xfer_asset: App.globalGet(Bytes("assetIDToken"))
-                      }),
-                      InnerTxnBuilder.Submit(),
-                      Approve()
-            ])
-            ).Else(Reject())
-
-    handle_noop=Seq([
-                cmd.store(Txn.application_args[0]),
-                Cond([cmd.load()==Bytes("sp"),handle_price("s")],
-                     [cmd.load()==Bytes("bp"),handle_price("b")],
-                     [cmd.load()==Bytes("b"),handle_buy],
-                     [cmd.load()==Bytes("s"),handle_start()],
-                ),Approve()]
-    )
+    #handle_noop=Seq([
+                #cmd.store(Txn.application_args[0]),
+                #Cond([cmd.load()==Bytes("sp"),handle_price("s")],
+                     #[cmd.load()==Bytes("bp"),handle_price("b")],
+                     #[cmd.load()==Bytes("b"),handle_buy],
+                     #[cmd.load()==Bytes("s"),handle_start()],
+                #),Approve()]
+    #)
 
     program = Cond(
         [Txn.application_id()==Int(0), handle_creation],
         [Txn.on_completion()==OnComplete.OptIn, handle_optin],
         [Txn.on_completion()==OnComplete.CloseOut, handle_closeout],
-        [Txn.on_completion()==OnComplete.UpdateApplication, handle_updateapp],
-        [Txn.on_completion()==OnComplete.DeleteApplication, handle_deleteapp],
-        [Txn.on_completion()==OnComplete.NoOp, handle_noop]
+        #[Txn.on_completion()==OnComplete.UpdateApplication, handle_updateapp],
+        #[Txn.on_completion()==OnComplete.DeleteApplication, handle_deleteapp],
+        #[Txn.on_completion()==OnComplete.NoOp, handle_noop]
     )
 
     return compileTeal(program, Mode.Application, version=5)
